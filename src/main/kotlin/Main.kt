dcee1f835af78f59aa47f409cc42b6f50f887ec7
winniemwangi <winniewameremwangi@gmail.com>
2022-02-12 07:54:18 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    var name = "winnie mwangi"
+    println(name)
+    var age = "23"
+    println(age)
+    var phonenumber = "0712345678"
+    println(phonenumber)
+    var weight = "39"
+    println(weight)
+
+    val iskenyan: Boolean = true
+    val isnotkenyan: Boolean = false
+    println(iskenyan)     // output true
+    println(isnotkenyan)  // output false
+}
